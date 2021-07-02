e940950a769dc59c8d9ccee4b96cc00625744069
Evgeniia <Jn.Golovina@gmail.com>
2021-07-02 18:49:54 +1200

    U: SVR

--- udemy/machine_learning_a-z_hands-on_python_&_r_in_data_science/R/polynomial_regression_template.R
+++ udemy/machine_learning_a-z_hands-on_python_&_r_in_data_science/R/polynomial_regression_template.R
@@ -1,5 +1,8 @@
 # Polynomial Regression
 
+# install.packages('ggplot2')
+library(ggplot2)
+
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
@@ -18,26 +21,23 @@ dataset = dataset[2:3]
 
 # Fitting Linear Regression to the dataset
 lin_reg = lm(formula = Salary ~ ., data = dataset)
+summary(lin_reg)
 
 # Fitting Polynomial Regression to the dataset
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
 dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ ., data = dataset)
+summary(poly_reg)
 
 # Visualising the Linear Regression results
-# install.packages('ggplot2')
-library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
   geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)), colour = 'blue') +
   ggtitle('Truth or Bluff (Linear Regression)') +
-  xlab('Level') +
-  ylab('Salary')
+  xlab('Level') + ylab('Salary')
 
 # Visualising the Polynomial Regression results
-# install.packages('ggplot2')
-library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
   geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)), colour = 'blue') +
@@ -46,10 +46,9 @@ ggplot() +
   ylab('Salary')
 
 # Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
-ggplot() + geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(poly_reg, newdata = data.frame(Level = x_grid,
                                                                        Level2 = x_grid^2,
                                                                        Level3 = x_grid^3,
