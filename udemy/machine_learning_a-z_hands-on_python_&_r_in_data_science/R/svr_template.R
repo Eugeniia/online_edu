f74173d0148e0b96e0d8160ab44f32d3031cd3b5
Evgeniia <Jn.Golovina@gmail.com>
2021-07-03 18:43:47 +1200

    U: svr minor changes

--- udemy/machine_learning_a-z_hands-on_python_&_r_in_data_science/R/svr_template.R
+++ udemy/machine_learning_a-z_hands-on_python_&_r_in_data_science/R/svr_template.R
@@ -1,4 +1,5 @@
-# SVR
+# Support Vector Regression
+library(e1071); library(ggplot2)
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
@@ -17,37 +18,23 @@ dataset = dataset[2:3]
 # test_set = scale(test_set)
 
 # Fitting SVR to the dataset
-# install.packages('e1071')
-library(e1071)
-regressor = svm(formula = Salary ~ .,
-                data = dataset,
-                type = 'eps-regression',
-                kernel = 'radial')
+regressor = svm(formula = Salary ~ ., data = dataset, type = 'eps-regression', kernel = 'radial')
+summary(regressor)
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
 # Visualising the SVR results
-# install.packages('ggplot2')
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
-            colour = 'blue') +
+ggplot() + geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)), colour = 'blue') +
   ggtitle('Truth or Bluff (SVR)') +
   xlab('Level') +
   ylab('Salary')
 
 # Visualising the SVR results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
-            colour = 'blue') +
+ggplot() + geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))), colour = 'blue') +
   ggtitle('Truth or Bluff (SVR)') +
   xlab('Level') +
   ylab('Salary')
